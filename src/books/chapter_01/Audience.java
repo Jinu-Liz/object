f216c3d3fbe3faceefb911b606a5706ac52ed9cd
hsyoon <bagzein@gmail.com>
2023-07-29 15:26:03 +0900

    1장 예제 : 티켓 판매 애플리케이션 구현

---
+++ src/books/chapter_01/Audience.java
@@ -0,0 +1,14 @@
+package books.chapter_01;
+
+public class Audience {
+
+  private Bag bag;
+
+  public Audience(Bag bag) {
+    this.bag = bag;
+  }
+
+  public Bag getBag() {
+    return bag;
+  }
+}
---
+++ src/books/chapter_01/Theater.java
@@ -0,0 +1,30 @@
+package books.chapter_01;
+
+public class Theater {
+
+  private TicketSeller ticketSeller;
+
+  public Theater(TicketSeller ticketSeller) {
+    this.ticketSeller = ticketSeller;
+  }
+
+  /**
+   * 해당 메서드를 이해하기 위해서는
+   * 1. Audience가 Bag을 가지고 있고
+   * 2. Bag 안에는 현금과 티켓이 들어있으며
+   * 3. TicketSeller가 TicketOffice에서 티켓을 판매하고
+   * 4. TicketOffice 안에 돈과 티켓이 보관돼 있다는 사실을 전부 알고 있어야 한다.
+   * 즉, 하나의 클래스 or 메서드가 너무 많은 세부사항을 다루고 있음.
+   */
+  public void enter(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
+  }
+}
