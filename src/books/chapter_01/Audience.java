ff3fc025846bd639324bd933caa296d9fb01bc91
hsyoon <bagzein@gmail.com>
2023-07-29 16:05:58 +0900

    - Audience 캡슐화 - TicketSeller와 Audience 사이의 결합도가 낮아짐. 자율적인 존재가 됨

--- src/books/chapter_01/Audience.java
+++ src/books/chapter_01/Audience.java
@@ -1,5 +1,9 @@
 package books.chapter_01;
 
+/**
+ * Audience가 자신의 가방에서 초대장을 스스로 확인하도록 변경.
+ * 외부의 제 3자가 자신의 가방을 열어보도록 허용하지 않는다.
+ */
 public class Audience {
 
   private Bag bag;
@@ -11,4 +15,20 @@ public class Audience {
   public Bag getBag() {
     return bag;
   }
+
+  public Long buy(Ticket ticket) {
+    Long fee = 0L;
+    if (this.bag.hasInvitation()) {
+      this.bag.setTicket(ticket);
+
+    } else {
+      this.bag.setTicket(ticket);
+      this.bag.minusAmount(ticket.getFee());
+
+      fee = ticket.getFee();
+    }
+
+    return fee;
+  }
+
 }
