e9dfc2a99bc1c502ff678948e725043ffde3a344
Hyungseock-Yun <bagzein@gmail.com>
2023-08-01 03:37:11 +0900

    - 객체를 사용하여 개념을 명시적으로 구현

--- src/main/java/ex/books/chapter_02/Money.java
+++ src/main/java/ex/books/chapter_02/Money.java
@@ -2,6 +2,11 @@ package ex.books.chapter_02;
 
 import java.math.BigDecimal;
 
+/**
+ * Long 타입 같은 변수는 Money 타입처럼 저장하는 값이 금액과 관련있다는 의미를 전달할 수 없다.
+ * 또한 금액과 관련된 로직이 다른 곳에 중복되어 구현되는 것을 막을 수 없다.
+ * 의미를 좀 더 명시적이고 분명하게 표현할 수 있다면 객체를 사용하여 해당 개념을 구현.
+ */
 public class Money {
 
   public static final Money ZERO = Money.wons(0);
