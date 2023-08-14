cc3511e8c53fd2d61ce4d554af75c526bff3b5b4
Hyungseock-Yun <bagzein@gmail.com>
2023-08-14 23:17:32 +0900

    - 또 다른 문제점

--- src/main/java/ex/books/chapter_05/Movie.java
+++ src/main/java/ex/books/chapter_05/Movie.java
@@ -60,6 +60,11 @@ public class Movie {
   /**
    * conditions가 나눠지면서, Movie가 협력해야하는 클래스가 두 개로 나위어졌다. (DiscountCondition -> SequenceCondition, PeriodCondition)
    * 가장 쉽게 해당 문제를 해결하는 방법은 각 클래스들의 목록을 별도로 유지하는 것이다.
+   * 하지만, 이 방법은 PeriodCondition과 SequenceCondition 양쪽 모두에게 결합된다. 클래스 분리 후, 전체적인 결합도가 높아졌다.
+   * 또한 수정 후에 새로운 할인 조건을 추가하기가 더 어려워졌다.
+   * 새로운 할인 조건 클래스를 담기 위한 List를 인스턴스 변수로 추가해야 한다.
+   * 이후, List를 이용하여 할인 조건을 만족하는지 여부를 판단하는 메서드도 추가해야한다.
+   * 마지막으로 이 메서드를 호출하도록 isDiscountable 메서드를 수정해야 한다.
    */
   private boolean checkPeriodConditions(Screening screening) {
     return periodConditions.stream()
