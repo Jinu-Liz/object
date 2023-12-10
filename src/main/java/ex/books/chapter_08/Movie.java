d52f4dece3ac6550a8f75112e5407a08e20012dd
hsyoon <bagzein@gmail.com>
2023-12-10 15:44:16 +0900

    - 퍼블릭 인터페이스를 통한 명시적인 의존성 사용

--- src/main/java/ex/books/chapter_08/Movie.java
+++ src/main/java/ex/books/chapter_08/Movie.java
@@ -3,13 +3,11 @@ package ex.books.chapter_08;
 
 import ex.books.chapter_08.policy.DiscountPolicy;
 import ex.books.common.Money;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Duration;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Movie {
@@ -40,6 +38,29 @@ public class Movie {
     this.title = title;
     this.runningTime = runningTime;
     this.fee = fee;
+  }
+
+  /**
+   * 생성자의 DiscountPolicy 인자가 추상 클래스 타입으로 선언됐기 때문에 객체를 생성할 때, 자식 클래스 중 어떤 것이라도 전달할 수 있다.
+   * 따라서 런타임에 AmountDiscountPolicy 또는 PercentDiscountPolicy의 인스턴스를 선택적으로 전달할 수 있다.
+   * 이는 수정자 주입 방식과 필드 주입 방식의 경우에도 동일하며, 모든 경우에 의존성은 명시적으로 퍼블릭 인터페이스에 노출된다.
+   * 이를 명시적인 의존성(explicit dependency)이라고 부른다.
+   *
+   * 반면, Movie의 내부에서 AmountDiscountPolicy의 인스턴스를 직접 생성하는 방식은 Movie가 DiscountPolicy에 의존한다는 사실을 감춘다.
+   * 즉, 의존성이 퍼블릭 인터페이스에 표현되지 않으며 이를 숨겨진 의존성(hidden dependency)이라고 부른다.
+   *
+   * 의존성이 명시적이지 않으면 의존성을 파악하기 위해 내부 구현을 직접 살펴보아야한다.
+   * 이를 파악하는 것은 쉽지 않을 뿐더러, 클래스를 다른 컨텍스트에서 재사용하기 위하여 내부 구현을 직접 변경해야 한다는 것이다.
+   * 이는 또다른 버그 발생 가능성을 내포한다.
+   *
+   * 따라서 의존성을 퍼블릭 인터페이스를 통해 명시적으로 드러내야하며,
+   * 그래야 퍼블릭 인터페이스를 통해 컴파일타임 의존성을 적절한 런타임 의존성으로 교체할 수 있다.
+   */
+  public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+    this.title = title;
+    this.runningTime = runningTime;
+    this.fee = fee;
+    this.discountPolicy = discountPolicy;
   }
 
   public Money calculateMovieFee(Screening screening) {
