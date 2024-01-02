902d73b802a29ef5082639009a364dfdbe6d9499
Hyungseock-Yun <bagzein@gmail.com>
2024-01-02 23:52:07 +0900

    - Screening 경로 수정

--- src/main/java/ex/books/chapter_09/policy/NoneDiscountPolicy.java
+++ src/main/java/ex/books/chapter_09/policy/NoneDiscountPolicy.java
@@ -1,6 +1,6 @@
 package ex.books.chapter_09.policy;
 
-import ex.books.chapter_08.Screening;
+import ex.books.chapter_09.Screening;
 import ex.books.common.Money;
 
 public class NoneDiscountPolicy implements DiscountPolicy {
