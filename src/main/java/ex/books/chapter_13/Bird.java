7ddb978817f224b221e73e769978a4dfb3afe95e
Hyungseock-Yun <bagzein@gmail.com>
2024-06-03 23:05:35 +0900

    - 서브클래싱과 서브타이핑 > is-a 관계

---
+++ src/main/java/ex/books/chapter_13/Bird.java
@@ -0,0 +1,7 @@
+package ex.books.chapter_13;
+
+public class Bird {
+  public void fly() {
+    System.out.println("Bird is flying");
+  }
+}
---
+++ src/main/java/ex/books/chapter_13/Client.java
@@ -0,0 +1,8 @@
+package ex.books.chapter_13;
+
+public class Client {
+  public static void main(String[] args) {
+    Penguin penguin = new Penguin();
+    penguin.fly();
+  }
+}
